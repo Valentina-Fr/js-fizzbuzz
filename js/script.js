706bc82c7d81eb567e330c52bbd9369d7eb1aaa1
Valentina-Fr <v_frasca@virgilio.it>
2021-07-05 15:37:58 +0200

    Add if statement inside for loop

--- js/script.js
+++ js/script.js
@@ -7,5 +7,17 @@ Per i multipli sia di 3 che di 5 stampare FizzBuzz.
 
 //Stampare numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
-    console.log(i)
+   if (i % 3 === 0 && i % 5 === 0) {
+       //Stampare FizzBuzz per multipli di 3 e 5
+       console.log("FizzBuzz")
+   } else if (i % 3 === 0) {
+       //Stampare Fizz per multipli di 3
+       console.log("Fizz")
+   } else if (i % 5 === 0) {
+       //Stampare Buzz per multipli di 5
+       console.log("Buzz")
+   } else {
+       //Stampare altri numeri
+       console.log(i)
+   }
 }
