26dd1728891d2a3bbb5c99a821019909120af7f8
Valentina-Fr <v_frasca@virgilio.it>
2021-07-05 15:52:47 +0200

    Print into html

--- js/script.js
+++ js/script.js
@@ -5,19 +5,26 @@ Per i multipli di 5 stampare Buzz
 Per i multipli sia di 3 che di 5 stampare FizzBuzz.
 */
 
+//Dichiarazione e assegnazione variabili
+var numbersPlaceholder = document.getElementById("numbers");
+var listItem = "";
+
 //Stampare numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
    if (i % 3 === 0 && i % 5 === 0) {
        //Stampare FizzBuzz per multipli di 3 e 5
-       console.log("FizzBuzz")
+       listItem += "<li>" + "FizzBuzz" + "</li>";
    } else if (i % 3 === 0) {
        //Stampare Fizz per multipli di 3
-       console.log("Fizz")
+       listItem += "<li>" + "Fizz" + "</li>";
    } else if (i % 5 === 0) {
        //Stampare Buzz per multipli di 5
-       console.log("Buzz")
+       listItem += "<li>" + "Buzz" + "</li>";
    } else {
        //Stampare altri numeri
-       console.log(i)
+       listItem += "<li>" + i + "</li>";
    }
 }
+
+//Stampare nell'html
+numbersPlaceholder.innerHTML = listItem;
