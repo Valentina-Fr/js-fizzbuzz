36ac043f7f561f8fcc7e1c8c967f06902a75258b
Valentina-Fr <v_frasca@virgilio.it>
2021-07-05 15:29:58 +0200

    Print number from 1 to 100

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* 
+1 - Scrivere un programma che stampi i numeri da 1 a 100
+Per i multipli di 3 stampare Fizz
+Per i multipli di 5 stampare Buzz
+Per i multipli sia di 3 che di 5 stampare FizzBuzz.
+*/
+
+//Stampare numeri da 1 a 100
+for (var i = 1; i <= 100; i++) {
+    console.log(i)
+}
